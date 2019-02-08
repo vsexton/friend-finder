8e508953331c9e07783cb71eef56aecb5cf41eef
majolex <majolex@matts-MacBook-Air.local>
2019-02-08 13:30:14 -0700

    added things

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+//express 
+var app = express();
+var PORT = process.env.PORT;
+
+//expose the public derectory to access css file 
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// Add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
