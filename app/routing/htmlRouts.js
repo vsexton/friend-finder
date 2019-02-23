d6c9b9f59a3a6c497a4a068a7d3ef1bcf7b96c9f
majolex <majolex@matts-MacBook-Air.local>
2019-02-23 12:17:17 -0700

    added things

--- app/routing/htmlRouts.js
+++ app/routing/htmlRouts.js
@@ -14,11 +14,11 @@ module.exports = function(app) {
     // In each of the below cases the user is shown an HTML page of content
     // ---------------------------------------------------------------------------
   
-    app.get("/tables", function(req, res) {
+    app.get("/", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/home.html"));
     });
   
-    app.get("/reserve", function(req, res) {
+    app.get("/survey", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
   
