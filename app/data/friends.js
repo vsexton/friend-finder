2f6f30640f4c6fbf6da4b32392fe083eb2ca3957
majolex <majolex@matts-MacBook-Air.local>
2019-02-23 21:37:31 -0700

    added things

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
 var friendsArray = [
 	{
 		name: 'Mickey',
-		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiN2YfyhdHgAhWnsVQKHSvBAgQQjRx6BAgBEAU&url=https%3A%2F%2Fwww.eonline.com%2Fnews%2F810527%2Fhappy-birthday-mickey-10-facts-you-might-not-know-about-the-famous-mouse&psig=AOvVaw2bH--I93PTTnc5TtHJl9G3&ust=1550983488994365',
+		photo: 'mickey.gif',
 		scores: [
 			'1',
 			'5',
@@ -18,7 +18,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Pluto',
-		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwil8_SNhtHgAhUCB3wKHbVVCqcQjRx6BAgBEAU&url=https%3A%2F%2Fwww.collectors.com%2Fentertainment-item%2Fpluto-disney-mickey-mouse-cartoon-dog-cardboard-cutout-stan%2F4313476438542890234&psig=AOvVaw2VzohSTecPYgrCjN7_auKt&ust=1550983545526230',
+		photo: 'pluto.gif',
 		scores: [
 			'2',
 			'4',
@@ -34,7 +34,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Minnie',
-		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi0ia6shtHgAhWEMXwKHVjCD30QjRx6BAgBEAU&url=https%3A%2F%2Feconomictimes.indiatimes.com%2Fmagazines%2Fpanache%2F90th-birthday-present-minnie-mouse-to-get-her-own-star-on-hollywood-walk-of-fame%2Farticleshow%2F62471080.cms&psig=AOvVaw2q9iHMrO-PJeIOpHSby2Bz&ust=1550983603609899',
+		photo: 'minnie.gif',
 		scores: [
 			'4',
 			'3',
@@ -50,7 +50,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Daisy',
-		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi2vvDVhtHgAhUnr1QKHa3XC2gQjRx6BAgBEAU&url=https%3A%2F%2Fwww.homedepot.com%2Fp%2FRoomMates-5-in-x-19-in-Mickey-and-Friends-Daisy-Duck-8-Piece-Peel-and-Stick-Giant-Wall-Decal-RMK1513GM%2F202984819&psig=AOvVaw0OmEcDNDDd16Iy0waiN4Sv&ust=1550983664553694',
+		photo: 'daisy.gif',
 		scores: [
 			'5',
 			'1',
@@ -66,7 +66,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Donald',
-		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiDy5a3i9HgAhXBy1QKHUr2AzwQjRx6BAgBEAU&url=https%3A%2F%2Fwww.orlandoweekly.com%2FBlogs%2Farchives%2F2014%2F06%2F09%2Fhappy-birthday-donald-duck&psig=AOvVaw0kIk4sPXEPD-qAogaOZYVO&ust=1550984439136203',
+		photo: 'donald.gif',
 		scores: [
 			'1',
 			'1',
@@ -82,7 +82,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Goofy',
-		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjm4-Dsi9HgAhXHxlQKHdHpCM8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.homedepot.com%2Fp%2FRoomMates-5-in-19-in-Mickey-and-Friends-Goofy-13-Piece-Peel-and-Stick-Giant-Wall-Decal-RMK1510GM%2F202984813&psig=AOvVaw1qngWwODWRDe-EIkZFpkXT&ust=1550985089710408',
+		photo: 'goffy.gif',
 		scores: [
 			'1',
 			'1',
@@ -98,7 +98,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Pete',
-		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj-9uSDjNHgAhWQ51QKHY6PAhkQjRx6BAgBEAU&url=https%3A%2F%2Falchetron.com%2FPete-(Disney)&psig=AOvVaw0oJCw7VRppjJqxC5KpyPog&ust=1550985135913512',
+		photo: 'pete.gif',
 		scores: [
 			'5',
 			'4',
@@ -114,7 +114,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Ludwig Von Drake',
-		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjgvOiljNHgAhUC_1QKHT9NDkgQjRx6BAgBEAU&url=https%3A%2F%2Fdisney.fandom.com%2Fwiki%2FLudwig_Von_Drake&psig=AOvVaw2WJcSjimpjvCJ6FVLv_ZvN&ust=1550985209349096',
+		photo: 'proffesor.gif',
 		scores: [
 			'5',
 			'2',
@@ -130,7 +130,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Clarabelle Cow',
-		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiM_9PBjNHgAhUPDHwKHWtFCMcQjRx6BAgBEAU&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FClarabelle_Cow&psig=AOvVaw0pVgdT4MDLZQuuGGgKEdjc&ust=1550985248892718',
+		photo: 'Clarabella.jpg',
 		scores: [
 			'5',
 			'5',
@@ -146,7 +146,7 @@ var friendsArray = [
 	},
 	{
 		name: 'Chip and Dale',
-		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjFpfvVjNHgAhWpsVQKHU-QDToQjRx6BAgBEAU&url=https%3A%2F%2Fdisney.fandom.com%2Fwiki%2FChip_and_Dale&psig=AOvVaw3hYG_m5SoItdARLuNmYv34&ust=1550985302724366',
+		photo: 'chipanddale.gif',
 		scores: [
 			'1',
 			'1',
